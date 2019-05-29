79eb24ecf4667668f9fdce709b66d770d1bed1e5
armayndo <armayndo1@yahoo.com>
2019-05-29 07:44:56 +0700

    balikin sebelumnya

--- TestBankAccount.java
+++ TestBankAccount.java
@@ -1,11 +1,10 @@
-package com;
+package ca.uwo.csd.cs2212.USERNAME;
 
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestBankAccount {
-	
 	@Test
 	public void testDebitWithSufficientFunds() {
 		BankAccount account = new BankAccount(10);
@@ -18,5 +17,4 @@ public class TestBankAccount {
 		double amount = account.debit(11);
 		Assert.assertEquals(10.0, amount);
 	}
-
 }
